3b7a2de68f5726ead969e0d25f1d447ba78cf5ca
jayjariwala <jay.jariwala@hotmail.com>
2020-08-25 15:38:08 -0400

    feat: global font-size change and media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react"
-import Styles from "./styles/Header.module.css"
+import Styles from "./styles/Navbar.module.css"
+import logo from "../assets/images/logo.png"
+import mobileLogo from "../assets/images/mobile-logo.png"
 
 const Header = props => {
   return (
     <nav className={Styles.Nav}>
       <div className="container">
         <div className={Styles.navLinks}>
-          <a className="">Navbar</a>
+          <a href="">
+            <img src={logo} width="180px" className={Styles.Logo} />
+            <img src={mobileLogo} width="80px" className={Styles.mobileLogo} />
+          </a>
           <ul>
             <li>Home</li>
             <li>Projects</li>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import { PropTypes } from "prop-types"
-import "./styles/global.css";
-import Styles from './styles/Layout.module.css';
-import Header from './Header';
+import "./styles/global.css"
+import Styles from "./styles/Layout.module.css"
+import Header from "./Header"
 
 class Layout extends React.Component {
   render() {
-    let {children} = this.props;
-    return(
-    <div className={Styles.Layout}>
-      <Header />
-      {children}
-      <h1>This is footer</h1>
-    </div>
+    let { children } = this.props
+    return (
+      <div className={Styles.Layout}>
+        <Header />
+        {children}
+        <h1>This is footer</h1>
+      </div>
     )
   }
 }
