b658453aa8d9028fa9955c4792fad948cad638ca
jayjariwala <jay.jariwala@hotmail.com>
2020-08-28 21:24:59 -0400

    feat: mobile nav hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import Styles from "./styles/Navbar.module.css"
 import logo from "../assets/images/logo.png"
 import mobileLogo from "../assets/images/mobile-logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Header = props => {
   return (
@@ -10,8 +12,17 @@ const Header = props => {
         <div className={Styles.navLinks}>
           <a href="#">
             <img src={logo} width="180px" className={Styles.Logo} />
-            <img src={mobileLogo} width="100px" className={Styles.mobileLogo} />
           </a>
+          <div className={Styles.mobileNav}>
+            <a>
+              <img
+                src={mobileLogo}
+                width="90px"
+                className={Styles.mobileLogo}
+              />
+            </a>
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          </div>
           <ul>
             <li>Home</li>
             <li>Projects</li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,10 @@
 import React, { Fragment } from "react"
 import Layout from "../components/Layout"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faCoffee,
-  faAddressBook,
-  faAirFreshener,
-  faAmbulance,
-  faAtom,
-  faBus,
-  faCoins,
-  faDice,
-} from "@fortawesome/free-solid-svg-icons"
 
 export default function Home() {
   return (
     <Layout>
       <div>Hello world!</div>
-      <div>
-        <FontAwesomeIcon icon={faCoffee} size="1x" />
-        <FontAwesomeIcon icon={faAddressBook} size="2x" />
-        <FontAwesomeIcon icon={faAirFreshener} size="3x" />
-        <FontAwesomeIcon icon={faAtom} size="4x" />
-        <FontAwesomeIcon icon={faAmbulance} size="5x" />
-        <FontAwesomeIcon icon={faBus} size="6x" />
-        <FontAwesomeIcon icon={faCoins} size="7x" />
-        <FontAwesomeIcon icon={faDice} size="8x" />
-      </div>
     </Layout>
   )
 }
